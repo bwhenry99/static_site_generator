5c1f74de018fee2345307f155f6162ca9e72a4df
bwhenry <bwhenry19@gmail.com>
2026-05-03 14:27:16 -0600

    rename files, add regex checks

--- src/test_textinline.py
+++ src/test_textinline.py
@@ -1,10 +1,10 @@
 import unittest
 
 from textnode import TextNode, TextType
-from splitnodesdelimiter import split_nodes_delimiter
+from textinline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
-class TestSplitNode(unittest.TestCase):
+class TestTextInline(unittest.TestCase):
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
@@ -83,5 +83,25 @@ class TestSplitNode(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://wikipedia.org"),
+            ],
+            matches,
+        )
+
+        
 if __name__ == "__main__":
     unittest.main()
