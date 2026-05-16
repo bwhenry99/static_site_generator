63b452115037822611a03cd4be956a6454c673aa
bwhenry <bwhenry19@gmail.com>
2026-05-16 16:25:02 -0600

    add splitters for images and links

--- src/textinline.py
+++ src/textinline.py
@@ -33,3 +33,41 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        currentText = node.text
+        images = extract_markdown_images(node.text)
+        for image in images:
+            sections = currentText.split(f"![{image[0]}]({image[1]})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            if len(sections) == 2:
+                currentText = sections[1]
+        if currentText:
+            new_nodes.append(TextNode(currentText, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        currentText = node.text
+        links = extract_markdown_links(node.text)
+        for link in links:
+            sections = currentText.split(f"[{link[0]}]({link[1]})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            if len(sections) == 2:
+                currentText = sections[1]
+        if currentText:
+            new_nodes.append(TextNode(currentText, TextType.TEXT))
+    return new_nodes
